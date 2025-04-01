b2e6c87a9deb6619b29dc24b829729a5f632c19d
Baby.Xeno <djm7992@psu.edu>
2025-04-01 00:48:53 -0400

    Update HealthWarning.ts
    fixed low hp warning box position

--- src/HealthWarning.ts
+++ src/HealthWarning.ts
@@ -65,7 +65,7 @@ class HealthWarning {
 
 		// Example
 		this.warningElement.style.top = "742px";
-		this.warningElement.style.left = "230px";
+		this.warningElement.style.left = "350px";
 
 		//OR use transform use somethin like, this.warningElement.style.transform = `translate(20px, 20px)`; 
 	}
