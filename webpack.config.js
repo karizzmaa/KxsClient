20f4b6d436eaa96500ff7c9ebd0d76f784ebe767
Anaïs Saraiva <53227802+Kisakay@users.noreply.github.com>
2025-04-04 18:31:12 +0200

    .

--- webpack.config.js
+++ webpack.config.js
@@ -4,42 +4,49 @@ const TerserPlugin = require('terser-webpack-plugin');
 const { banner } = require('./banner');
 const config = require('./config.json');
 module.exports = {
-  entry: './src/index.ts',
-  output: {
-    filename: config.fileName,
-    path: path.resolve(__dirname, 'dist'),
-  },
-  resolve: {
-    extensions: ['.ts', '.js'],
-  },
-  module: {
-    rules: [
-      {
-        test: /\.ts$/,
-        use: 'ts-loader',
-        exclude: /node_modules/,
-      },
-    ],
-  },
-  mode: 'production',
-  devtool: false,
-  optimization: {
-    minimize: false,
-    minimizer: [
-      new TerserPlugin({
-        terserOptions: {
-          format: {
-            comments: /@name|@namespace|@version|@description|@author|@license|@require|@run-at|@match|@grant|@downloadURL|@updateURL|==UserScript==|==\/UserScript==/,
-          },
-        },
-        extractComments: false,
-      }),
-    ],
-  },
-  plugins: [
-    new webpack.BannerPlugin({
-      banner: banner,
-      raw: true,
-    }),
-  ],
+	entry: './src/index.ts',
+	output: {
+		filename: config.fileName,
+		path: path.resolve(__dirname, 'dist'),
+	},
+	resolve: {
+		extensions: ['.ts', '.js'],
+	},
+	module: {
+		rules: [
+			{
+				test: /\.ts$/,
+				use: {
+					loader: 'ts-loader',
+					options: {
+						compilerOptions: {
+							noEmit: false
+						}
+					}
+				},
+				exclude: /node_modules/,
+			},
+		],
+	},
+	mode: 'production',
+	devtool: false,
+	optimization: {
+		minimize: false,
+		minimizer: [
+			new TerserPlugin({
+				terserOptions: {
+					format: {
+						comments: /@name|@namespace|@version|@description|@author|@license|@require|@run-at|@match|@grant|@downloadURL|@updateURL|==UserScript==|==\/UserScript==/,
+					},
+				},
+				extractComments: false,
+			}),
+		],
+	},
+	plugins: [
+		new webpack.BannerPlugin({
+			banner: banner,
+			raw: true,
+		}),
+	],
 };
